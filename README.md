8aab5aa43d6f2d989d58349712149654d14ff1e1
liangxin1300 <XLiang@suse.com>
2019-03-29 15:52:27 +0800

    update version to 0.0.11

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ pip3 install cluster_test_tool
 
 ## Use
 cluster_test_tool --help
-(for specific option, root and it's password is needed)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cluster_test_tool",
-    version="0.0.10",
+    version="0.0.11",
     author="Xin Liang",
     author_email="XLiang@suse.com",
     packages=setuptools.find_packages(),
