c93f05dc870706095f39f070379561dca64ffd9a
liangxin1300 <XLiang@suse.com>
2019-05-29 14:19:45 +0800

    test1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cluster_test_tool",
-    version="0.0.20",
+    version="0.0.21",
     author="Xin Liang",
     author_email="XLiang@suse.com",
     url="https://github.com/liangxin1300/clusterTestTool",
