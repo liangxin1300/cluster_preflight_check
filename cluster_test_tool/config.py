ac49b6dd4d858119b1f2ee3e43bd8670c5736b74
liangxin1300 <XLiang@suse.com>
2019-06-13 13:58:35 +0800

    Dev: remove unuseful code

--- cluster_test_tool/config.py
+++ cluster_test_tool/config.py
@@ -2,12 +2,6 @@
 pacemaker2_daemons = ("pacemaker-based", "pacemaker-fenced", "pacemaker-execd",
                     "pacemaker-attrd", "pacemaker-schedulerd", "pacemaker-controld")
 
-expected_resources = {
-    'sbd':        ('Restart|Fenced', 'Fenced'),
-    'corosync':   ('Restart|Fenced', 'Fenced'),
-    'pacemakerd': ('Restart', None),
-}
-
 
 RESTART_TIMEOUT = 5
 FENCE_TIMEOUT = 60
@@ -20,30 +14,4 @@ PASS_ASK = False
 MASK = False
 LOOP = False
 DEBUG = False
-
-
-class Option(object):
-    def __init__(self, name, alias, expect, systemd):
-        from . import utils
-        if utils.is_pacemaker_1() and alias != '':
-            self.name = alias
-        else:
-            self.name = name
-        self.alias = alias
-        self.expect = expect
-        self.mask_cmd = "systemctl mask {} --runtime".format(systemd)
-        self.unmask_cmd = "systemctl unmask {} --runtime".format(systemd)
-        # killall doesn't work for long command, see man killall
-        if len(self.name) > 15:
-            self.command = "kill -9 `pidof {}`".format(self.name)
-        else:
-            self.command = "killall -9 " + self.name
-        self.option = "--kill-{}".format(self.alias if self.alias else self.name)
-        self.dest = self.alias if self.alias else self.name
-        self.help = "kill {}{} daemon".\
-                    format(self.name, "({})".format(self.alias) if self.alias else "")
-
-    def __str__(self):
-        return "name: {}\nalias: {}\ncommand: {}\noption: {}\nhelp: {}\n\n".\
-               format(self.name, self.alias, self.command, self.option, self.help)
 
